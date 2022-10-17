180c050f8fe288355f66aa1eef45f4ff05b52d49
gustavojb1 <gustavojunqueirab@gmail.com>
2022-10-17 15:54:10 -0300

    finished

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const FlexContainer = styled.div`
   flex-wrap: wrap;
 `
 function App() {
-  const [pokemon, setPokemon] = useState({
+  const [pokemon1, setPokemon1] = useState({
     name: "Pichu",
     type: "Electric",
     evolved: false,
@@ -23,6 +23,15 @@ function App() {
     image: 'https://archives.bulbagarden.net/media/upload/thumb/b/b9/172Pichu.png/250px-172Pichu.png',
     id: 0
   })
+  const [pokemon2, setPokemon2] = useState({
+    name: "Charmander ",
+    type: "Fire",
+    evolved: false,
+    weight: 2,
+    color: 'red',
+    image: 'https://archives.bulbagarden.net/media/upload/archive/7/73/20091223030246%21004Charmander.png',
+    id: 1
+  })
 
   // Para fazer seus próximos pokemons, crie novos estados!
 
@@ -30,7 +39,8 @@ function App() {
   <GlobalStyles/>
     <FlexContainer>
       {/* Aqui neste componente, passe as props. Lembre-se que também é possivel passar a função de setState via props! */}
-      <PokemonCard/>
+      <PokemonCard pokemon={pokemon1} setPokemon={setPokemon1}/>
+      <PokemonCard pokemon={pokemon2} setPokemon={setPokemon2}/>
       {/* Crie aqui seus próximos pokemons! */}
     </FlexContainer>
   </>
--- src/Components/PokemonCard/PokemonCard.js
+++ src/Components/PokemonCard/PokemonCard.js
@@ -5,18 +5,38 @@ import {Card, PokemonName, PokemonType, EvolveButton} from './styles'
 
 const PokemonCard = (props) => {
 
-    const evoluirPokemon = () => {
+    const evoluirPokemon = (id) => {
         console.log("Cliquei no botão de evoluir")
+
+        if(id===0){
+          props.setPokemon({...props.pokemon, id:5, name:"Pikachu", weight:6, image:"https://archives.bulbagarden.net/media/upload/archive/0/0d/20100904002342%21025Pikachu.png" })
+        }
+        else if(id===5){
+          props.setPokemon({...props.pokemon, id:6, name:"Raichu", weight:25, image:"https://archives.bulbagarden.net/media/upload/archive/8/88/20100629124304%21026Raichu.png" })
+        }
+        else if(id===6){
+          alert("pokemon não tem mais evolução")
+        }
+    
+        else if(id===1){
+          props.setPokemon({...props.pokemon, id:3, name:"Charmeleon", weight:10, image:"https://archives.bulbagarden.net/media/upload/archive/4/4a/20130810021222%21005Charmeleon.png" })
+        }
+        else if(id===3){
+          props.setPokemon({...props.pokemon, id:4, name:"Charlizard", weight:150,  image:"https://archives.bulbagarden.net/media/upload/archive/7/7e/20130512093836%21006Charizard.png" })
+        }
+        else if(id===4){
+          alert("pokemon não tem mais evolução")
+        }
     }
     
   return (
-    <Card color={}>
-        <img src={} alt={`Pokemon`}/>
-        <PokemonName>{}</PokemonName>
-        <PokemonType>{}</PokemonType>
-        <p>{}kg</p>
+    <Card color={props.pokemon.color}>
+        <img src={props.pokemon.image} alt={`Pokemon`}/>
+        <PokemonName>{props.pokemon.name}</PokemonName>
+        <PokemonType>{props.pokemon.type}</PokemonType>
+        <p>{props.pokemon.weight}kg</p>
 
-        <EvolveButton onClick={() => evoluirPokemon()}>Evoluir!</EvolveButton>
+        <EvolveButton onClick={() => evoluirPokemon(props.pokemon.id)}>Evoluir!</EvolveButton>
     </Card>
   )
 }
